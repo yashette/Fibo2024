d340d6e2c9e5c24a5f82b32ba0030d16eabb4aa8
yashette <79081473+yashette@users.noreply.github.com>
2024-12-03 15:02:19 +0100

    docker

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,11 +1,20 @@
 using Leonardo;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World 2!");
 
-app.MapGet("/Fibonacci",
-    async () => await Fibonacci.RunAsync(new []{"44", "43"}));
+app.MapGet("/fibonacci", async () =>
+{
+    var result = await Fibonacci.RunAsync(["42"]);
+    return Results.Json(result, FibonacciContext.Default.ListFibonacciResult);
+});
 
 app.Run();
+
+[JsonSerializable(typeof(List<FibonacciResult>))]
+internal partial class FibonacciContext : JsonSerializerContext
+{
+}
