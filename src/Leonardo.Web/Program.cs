254f8f0dc2f5c4882f0b6e73ddbb98e8bf12aa64
yashette <79081473+yashette@users.noreply.github.com>
2024-12-17 16:48:35 +0100

    'swagger'

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -5,7 +5,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FibonacciDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<Fibonacci>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World 2!");
 
